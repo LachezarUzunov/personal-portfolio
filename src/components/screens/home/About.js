87178ab4e031be8ac53973d1b46cf489b5f99ef8
Lachezar Uzunov <uzunov_89_lacho@abv.bg>
2022-11-28 23:57:41 +0200

    CSS changes

---
+++ src/components/screens/home/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./About.module.css";
+
+const About = () => {
+  return (
+    <section className={classes.about}>
+      <div>
+        <h2>Me</h2>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/components/screens/home/Header.js
+++ src/components/screens/home/Header.js
@@ -4,20 +4,22 @@ import classes from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div className={classes.header}>
-      <h2>Hello, I am Lachezar Uzunov</h2>
-      <h2>A Frontend Software Engineer</h2>
-      <Line />
-      <h3 className={classes.desc}>
-        I am self-motivated, eager to learn new technologies and passionate
-        about building exciting projects! I am also a father, a hard rock lover
-        and an amateur guitarist
-      </h3>
-      <div className={classes.btns__div}>
-        <button className="btn__primary">Download CV</button>
-        <button>View My Projects</button>
+    <section className={classes.header__section}>
+      <div className={classes.header}>
+        <h2>Hello, I am Lachezar Uzunov</h2>
+        <h2>Frontend Software Engineer</h2>
+        <Line />
+        <h3 className={classes.desc}>
+          I am motivated and eager to learn new technologies and passionate
+          about building exciting projects! I am also a father, a hard rock
+          lover and an amateur guitarist
+        </h3>
+        <div className={classes.btns__div}>
+          <button className="btn__primary">Download CV</button>
+          <button>View My Projects</button>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
