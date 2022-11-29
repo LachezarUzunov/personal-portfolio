d58aa7b2ff7d6e1e64f4f882d3bddf0d069426dc
Lachezar Uzunov <uzunov_89_lacho@abv.bg>
2022-11-29 23:05:26 +0200

    adding skills

--- src/components/screens/home/Home.js
+++ src/components/screens/home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import About from "./About";
 import Header from "./Header";
+import ReactProjects from "./ReactProjects";
 
 const Home = () => {
   return (
     <React.Fragment>
       <Header />
       <About />
+      <ReactProjects />
     </React.Fragment>
   );
 };
---
+++ src/components/screens/home/ReactProjects.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ReactProjects = () => {
+  return (
+    <div>
+      <div>
+        <h4>My React Apps</h4>
+      </div>
+    </div>
+  );
+};
+
+export default ReactProjects;
