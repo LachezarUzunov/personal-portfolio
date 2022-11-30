975dbada68a4b0883275cda5c3298f4224d73581
Lachezar Uzunov <uzunov_89_lacho@abv.bg>
2022-11-30 17:43:23 +0200

    Adding Projects section

--- src/components/screens/home/About.js
+++ src/components/screens/home/About.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { AiFillHtml5 } from "react-icons/ai";
-import { FaAngular, FaCpanel, FaCss3, FaNode, FaNodeJs, FaReact, FaWordpressSimple } from "react-icons/fa";
+import {
+  FaAngular,
+  FaCpanel,
+  FaCss3,
+  FaNode,
+  FaNodeJs,
+  FaReact,
+  FaWordpressSimple,
+} from "react-icons/fa";
 import { MdDns } from "react-icons/md";
 import { SiJavascript, SiMongodb } from "react-icons/si";
 import Line from "../../layout/Line";
@@ -8,88 +16,91 @@ import classes from "./About.module.css";
 
 const About = () => {
   return (
-    <section className="container">
-      <div className={classes.about__heading}>
-        <h3>More about me and my experience</h3>
-        <Line />
-      </div>
-      <div className={classes.about__section}>
-        <div>
-          <img src={require("../../../assets/me.jpg")} alt="profile" />
-        </div>
-        <div>
-          <p>
-            I've been working in the IT industry for the past 8 years, 7 of
-            which as first line technical support representative at SiteGround -
-            one of the largest hosting services providers. I've helped over 60
-            000 clients with WordPress issues,{" "}
-          </p>
-          <h3>My Tech Stack</h3>
+    <section className={classes.about__bg}>
+      <section className="container">
+        <div className={classes.about__heading}>
+          <h2>More about me and my experience</h2>
           <Line />
-          <div className={classes.about__skills}>
-            <div>
-              <i className={classes.icon}>
-                <FaCpanel />
-              </i>
-              <h4>cPanel</h4>
+        </div>
+        <div className={classes.about__section}>
+          <div>
+            <img src={require("../../../assets/me.jpg")} alt="profile" />
+          </div>
+          <div>
+            <p>
+              I've been working in the IT industry for the past 8 years, 7 of
+              which as first line technical support representative at SiteGround
+              - one of the largest hosting services providers. I've helped over
+              60 000 clients with WordPress issues,{" "}
+            </p>
+            <h2>My Tech Stack</h2>
+            <Line />
+            <div className={classes.about__skills}>
+              <div>
+                <i className={classes.icon}>
+                  <FaCpanel />
+                </i>
+                <h4>cPanel</h4>
+              </div>
+              <div>
+                <i className={classes.icon}>
+                  <FaWordpressSimple />
+                </i>
+                <h4>WordPress</h4>
+              </div>
+              <div>
+                <i className={classes.icon}>
+                  <MdDns />
+                </i>
+                <h4>DNS</h4>
+              </div>
+              <div>
+                <i className={classes.icon}>
+                  <AiFillHtml5 />
+                </i>
+                <h4>HTML5</h4>
+              </div>
+              <div>
+                <i className={classes.icon}>
+                  <FaCss3 />
+                </i>
+                <h4>CSS3</h4>
+              </div>
+              <div>
+                <i className={classes.icon}>
+                  <SiJavascript />
+                </i>
+                <h4>JavaScript</h4>
+              </div>
+              <div>
+                <i className={classes.icon}>
+                  <FaNode />
+                </i>
+                <h4>NodeJS</h4>
+              </div>
+              <div>
+                <i className={classes.icon}>
+                  <FaReact />
+                </i>
+                <h4>ReactJS</h4>
+              </div>
+              <div>
+                <i className={classes.icon}>
+                  <FaAngular />
+                </i>
+                <h4>Angular</h4>
+              </div>
+              <div>
+                <i className={classes.icon}>
+                  <SiMongodb />
+                </i>
+                <h4>MongoDB</h4>
+              </div>
             </div>
-            <div>
-              <i className={classes.icon}>
-                <FaWordpressSimple />
-              </i>
-              <h4>WordPress</h4>
-            </div>
-            <div>
-              <i className={classes.icon}>
-                <MdDns />
-              </i>
-              <h4>DNS</h4>
-            </div>
-            <div>
-              <i className={classes.icon}>
-                <AiFillHtml5 />
-              </i>
-              <h4>HTML5</h4>
-            </div>
-            <div>
-              <i className={classes.icon}>
-                <FaCss3 />
-              </i>
-              <h4>CSS3</h4>
-            </div>
-            <div>
-              <i className={classes.icon}>
-                <SiJavascript />
-              </i>
-              <h4>JavaScript</h4>
-            </div>
-            <div>
-              <i className={classes.icon}>
-                <FaNode />
-              </i>
-              <h4>NodeJS</h4>
-            </div>
-            <div>
-              <i className={classes.icon}>
-                <FaReact />
-              </i>
-              <h4>ReactJS</h4>
-            </div>
-            <div>
-              <i className={classes.icon}>
-                <FaAngular />
-              </i>
-              <h4>Angular</h4>
-            </div>
-            <div>
-              <i className={classes.icon}>
-                <SiMongodb />
-              </i>
-              <h4>MongoDB</h4>
-            </div>
+            <button className="btn__secondary">Let's Talk</button>
           </div>
         </div>
-      </div>
+      </section>
     </section>
   );
 };
--- src/components/screens/home/ReactProjects.js
+++ src/components/screens/home/ReactProjects.js
@@ -1,12 +1,30 @@
 import React from "react";
+import Line from "../../layout/Line";
+import classes from "./ReactProjects.module.css";
 
 const ReactProjects = () => {
   return (
-    <div>
-      <div>
-        <h4>My React Apps</h4>
+    <section className={classes.projects__bg}>
+      <div className={classes.projects__header}>
+        <h2>My React Apps</h2>
+        <Line />
       </div>
-    </div>
+
+      <div className="container">
+        <section className={classes.projects__section}>
+          <article>
+            <h4>Technologies used</h4>
+            <div className={classes.projects__tech_stack}>
+              <h5>React</h5>
+              <h5>NodeJS</h5>
+            </div>
+          </article>
+          <article className={classes.projects__image}>
+            <img src={require("../../../assets/guitar.jpg")} alt="guitar" />
+          </article>
+        </section>
+      </div>
+    </section>
   );
 };
 
