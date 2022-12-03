394006e033c24c5083928d01476dbc982adddf53
Lachezar Uzunov <uzunov_89_lacho@abv.bg>
2022-12-03 23:52:05 +0200

    CSS updates

--- src/components/screens/home/About.js
+++ src/components/screens/home/About.js
@@ -25,7 +25,7 @@ const About = () => {
           <div>
             <img src={require("../../../assets/me.jpg")} alt="profile" />
           </div>
-          <div>
+          <div className={classes.about__section_text}>
             <p>
               I've been working in the IT industry for the past 8 years, 7 of
               which as first line technical support representative at SiteGround
@@ -33,74 +33,75 @@ const About = () => {
               60 000 clients with WordPress issues, DNS changes and settings as
               well as email clients configuration.{" "}
             </p>
+            <button className="btn__secondary">Let's Talk</button>
           </div>
         </div>
-        <div>
-        <h2 className={classes.about__skills_heading}>My Tech Stack</h2>
-            <Line />
-            <div className={classes.about__skills}>
-              <div>
-                <i className={classes.icon}>
-                  <FaCpanel />
-                </i>
-                <h4>cPanel</h4>
-              </div>
-              <div>
-                <i className={classes.icon}>
-                  <FaWordpressSimple />
-                </i>
-                <h4>WordPress</h4>
-              </div>
-              <div>
-                <i className={classes.icon}>
-                  <MdDns />
-                </i>
-                <h4>DNS</h4>
-              </div>
-              <div>
-                <i className={classes.icon}>
-                  <AiFillHtml5 />
-                </i>
-                <h4>HTML5</h4>
-              </div>
-              <div>
-                <i className={classes.icon}>
-                  <FaCss3 />
-                </i>
-                <h4>CSS3</h4>
-              </div>
-              <div>
-                <i className={classes.icon}>
-                  <SiJavascript />
-                </i>
-                <h4>JavaScript</h4>
-              </div>
-              <div>
-                <i className={classes.icon}>
-                  <FaNode />
-                </i>
-                <h4>NodeJS</h4>
-              </div>
-              <div>
-                <i className={classes.icon}>
-                  <FaReact />
-                </i>
-                <h4>ReactJS</h4>
-              </div>
-              <div>
-                <i className={classes.icon}>
-                  <FaAngular />
-                </i>
-                <h4>Angular</h4>
-              </div>
-              <div>
-                <i className={classes.icon}>
-                  <SiMongodb />
-                </i>
-                <h4>MongoDB</h4>
-              </div>
+        {/* SKILLS SECTION */}
+        <div className={classes.about__skills_heading}>
+          <h2 >My Tech Stack</h2>
+          <Line />
+          <div className={classes.about__skills}>
+            <div>
+              <i className={classes.icon}>
+                <FaCpanel />
+              </i>
+              <h4>cPanel</h4>
             </div>
-            <button className="btn__secondary">Let's Talk</button>
+            <div>
+              <i className={classes.icon}>
+                <FaWordpressSimple />
+              </i>
+              <h4>WordPress</h4>
+            </div>
+            <div>
+              <i className={classes.icon}>
+                <MdDns />
+              </i>
+              <h4>DNS</h4>
+            </div>
+            <div>
+              <i className={classes.icon}>
+                <AiFillHtml5 />
+              </i>
+              <h4>HTML5</h4>
+            </div>
+            <div>
+              <i className={classes.icon}>
+                <FaCss3 />
+              </i>
+              <h4>CSS3</h4>
+            </div>
+            <div>
+              <i className={classes.icon}>
+                <SiJavascript />
+              </i>
+              <h4>JavaScript</h4>
+            </div>
+            <div>
+              <i className={classes.icon}>
+                <FaNode />
+              </i>
+              <h4>NodeJS</h4>
+            </div>
+            <div>
+              <i className={classes.icon}>
+                <FaReact />
+              </i>
+              <h4>ReactJS</h4>
+            </div>
+            <div>
+              <i className={classes.icon}>
+                <FaAngular />
+              </i>
+              <h4>Angular</h4>
+            </div>
+            <div>
+              <i className={classes.icon}>
+                <SiMongodb />
+              </i>
+              <h4>MongoDB</h4>
+            </div>
+          </div>
         </div>
       </section>
     </section>
